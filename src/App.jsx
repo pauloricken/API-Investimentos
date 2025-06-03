66c40e3c951e8edeed6e3184c12177e2efe23f31
Paulo Vitor Ricken <104838237+pauloricken@users.noreply.github.com>
2025-06-03 20:07:03 -0300

    Add React import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
